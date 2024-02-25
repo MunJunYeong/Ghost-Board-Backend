3933cb1e750d63e8a727b19eb1a98fcce14f76e7
DESKTOP-G145FC7\moon <msw711666@naver.com>
2024-02-25 19:11:56 +0900

    .env setting

--- index.ts
+++ index.ts
@@ -1,24 +1,19 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
+import { GetEnvPath } from "./src/configs/common";
+
+import * as dotenv from "dotenv";
+dotenv.config({ path: GetEnvPath() });
 
 const app: Express = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+// console.log("path :", envPath);
 
-app.get('/', (req: Request, res: Response) => {
-  // business logic
-  
-  res.send('Express Server');
-});
-app.post('/', (req: Request, res: Response) => {
-  // business logic
-  
-  res.send('Express Server');
-});
-app.put('/', (req: Request, res: Response) => {
-  // business logic
-  
-  res.send('Express Server');
+app.get("/", (req: Request, res: Response) => {
+    // business logic
+
+    res.send("Express Server");
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at <https://localhost>:${port}`);
+    console.log(`[server]: Server is running at <https://localhost>:${port}`);
 });
