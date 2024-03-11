51f3d4d6e560d89ecf0dbcb68841fba512093775
DESKTOP-G145FC7\moon <msw711666@naver.com>
2024-03-11 21:10:15 +0900

    error middleware 추가 및 routes 등록

---
+++ src/middlewares/error.ts
@@ -0,0 +1,27 @@
+import { logger } from "@configs/logger";
+import { NextFunction, Request, Response } from "express";
+import { CustomError } from "@errors/custom";
+
+export const errorMiddleware = async (err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof CustomError) {
+        const { statusCode, message, stack, logging } = err;
+        if (logging) {
+            logger.error(
+                JSON.stringify(
+                    {
+                        code: statusCode,
+                        message: message,
+                        stack: stack,
+                    },
+                    null,
+                    2
+                )
+            );
+        }
+        return res.status(statusCode).send({ message: message });
+    }
+
+    // Unhandled errors
+    logger.error(JSON.stringify(err, null, 2));
+    return res.status(500).send({ errors: [{ message: "unhandled error occured" }] });
+};
