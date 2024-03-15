67706d2ff84fbc8a89feff65b1e00a33e4b9de3b
DESKTOP-G145FC7\moon <msw711666@naver.com>
2024-03-16 00:34:31 +0900

    add encryption unittest

---
+++ tests/utils/encryption.test.ts
@@ -0,0 +1,24 @@
+import { comparePassword, hashing } from "@src/common/utils/encryption";
+
+describe("encryption util", () => {
+    let pwd = "test1234";
+    let hashedPwd = "";
+
+    describe("성공", () => {
+        test("hashing", async () => {
+            hashedPwd = await hashing(pwd);
+            expect(hashedPwd).not.toEqual(pwd);
+        });
+        test("compared", async () => {
+            const isEqual = await comparePassword(pwd, hashedPwd);
+            expect(isEqual).toEqual(true);
+        });
+    });
+    describe("Exception", () => {
+        test("not matched password", async () => {
+            const wrongPwd = "wrongpassword";
+            const isEqual = await comparePassword(wrongPwd, hashedPwd);
+            expect(isEqual).toEqual(false);
+        });
+    });
+});
