252251fd50142b67a588a41b8a24fd2ad24695c1
MunJunYeong <msw711666@naver.com>
2024-03-15 16:23:42 +0900

    get user data api 추가 및 test case 추가

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
 
-import RedisClient, { Redis } from "@src/configs/redis";
-import UserService from "@src/services/user/user.service";
+import RedisClient, { Redis } from "@configs/redis";
+import UserService from "@services/user/user.service";
 import { ErrNotFound } from "@src/common/errors/custom";
 import BadRequestError from "@src/common/errors/bad_request";
 import InternalError from "@src/common/errors/internal_server";
+import User from "@models/user";
 
 export default class UserController {
     private redis: Redis;
@@ -15,6 +16,19 @@ export default class UserController {
 
         this.userService = new UserService();
     }
+
+    getUser = async (req: Request, res: Response) => {
+        const id = req.params.id;
+        try {
+            const user: User = await this.userService.getUser(id);
+            res.send({ message: `success get user (id : ${id})`, data: user.dataValues });
+        } catch (err: any) {
+            if (err.message === ErrNotFound) {
+                throw new BadRequestError({ code: 404, error: err });
+            }
+            throw new InternalError({ error: err });
+        }
+    };
 
     deleteUser = async (req: Request, res: Response) => {
         const id = req.params.id;
