271d31e3f9d755adc2a693b72f4ed064210b6585
MunJunYeong <msw711666@naver.com>
2024-03-20 14:32:08 +0900

    custom error를 extends한 error class에서 message 내용 추가

--- src/common/errors/bad_request.ts
+++ src/common/errors/bad_request.ts
@@ -6,10 +6,19 @@ export default class BadRequestError extends CustomError {
     private readonly _stack?: string;
     private readonly _logging: boolean;
 
-    constructor(params?: { code?: number; error: Error }) {
-        super(params?.error.message || "Bad request");
+    constructor(params?: { code?: number; error?: Error; message?: string }) {
+        let combinedMsg: string;
+        if (params?.message && params?.error) {
+            combinedMsg = `${params.message}: ${params.error.message}`;
+        } else if (params?.error) {
+            combinedMsg = params.error.message;
+        } else {
+            combinedMsg = params?.message || "Bad Request";
+        }
+
+        super(combinedMsg);
         this._code = params?.code || BadRequestError.DEFAULT_STATUS_CODE;
-        this._stack = params?.error.stack;
+        this._stack = params?.error?.stack;
         this._logging = true;
 
         // Only because we are extending a built in class
--- src/common/errors/internal_server.ts
+++ src/common/errors/internal_server.ts
@@ -6,10 +6,19 @@ export default class InternalError extends CustomError {
     private readonly _stack?: string;
     private readonly _logging: boolean;
 
-    constructor(params?: { code?: number; error: Error }) {
-        super(params?.error.message || "Internal server");
+    constructor(params?: { code?: number; error?: Error; message?: string }) {
+        let combinedMsg: string;
+        if (params?.message && params?.error) {
+            combinedMsg = `${params.message}: ${params.error.message}`;
+        } else if (params?.error) {
+            combinedMsg = params.error.message;
+        } else {
+            combinedMsg = params?.message || "Internal server error";
+        }
+
+        super(combinedMsg);
         this._code = params?.code || InternalError.DEFAULT_STATUS_CODE;
-        this._stack = params?.error.stack;
+        this._stack = params?.error?.stack;
         this._logging = true;
 
         Object.setPrototypeOf(this, InternalError.prototype);
