201ce4c210746b5ac42851d7349d36025aedf571
MUNJUNYEONG <msw711666@naver.com>
2024-03-25 20:01:31 +0900

    basic board repo 구현

---
+++ src/repository/board.repo.ts
@@ -0,0 +1,34 @@
+import Board from "@models/board"
+
+export default class BoardRepo {
+    constructor() { }
+
+    createBoard = async (board: Board) => {
+        try {
+            return await Board.create({
+                title: board.title,
+                description: board.description,
+            })
+        } catch (err) {
+            throw err
+        }
+    }
+    findBoardByID = async (id: string) => {
+        return await Board.findOne({
+            where: {
+                boardId: id,
+            },
+        });
+    }
+    findBoards = async () => {
+        return await Board.findAll();
+    }
+
+    deleteBoard = async (id: string) => {
+        return await Board.destroy({
+            where: {
+                boardId: id,
+            },
+        });
+    }
+}
