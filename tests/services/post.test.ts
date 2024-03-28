3aa1a2fcedfe7431e3d2c5e675bba76dfcaf5b0f
MUNJUNYEONG <msw711666@naver.com>
2024-03-28 21:38:14 +0900

    post service test 추가 및 전반적인 test refactoring

--- tests/services/post.test.ts
+++ tests/services/post.test.ts
@@ -41,57 +41,172 @@ const postTitle = "test post title";
 const postDesc = "test post description";
 let newPost: Post;
 
-describe("Create Post API", () => {
-    let body: CreatePostReqDTO;
-    beforeEach(() => {
-        body = {
-            title: postTitle,
-            description: postDesc,
-        };
-    });
-
-    describe("성공", () => {
-        test("Post - api/boards/:boardId/posts", async () => {
-            const response: any = await request(app)
-                .post(`/api/boards/${board.boardId}/posts`)
-                .set("Authorization", `Bearer ${accessToken}`)
-                .send(body);
-            expect(response.statusCode).toBe(200);
-            newPost = response.body.data;
+describe("Post API", () => {
+    describe("Create Post API", () => {
+        let body: CreatePostReqDTO;
+        beforeEach(() => {
+            body = {
+                title: postTitle,
+                description: postDesc,
+            };
         });
-    });
-    describe("Exception", () => {
-        test("invalid title - empty, length", async () => {
-            async function testTitleLength(title: string, expectedStatusCode: number) {
+
+        describe("성공", () => {
+            test("Post - api/boards/:boardId/posts", async () => {
+                const response: any = await request(app)
+                    .post(`/api/boards/${board.boardId}/posts`)
+                    .set("Authorization", `Bearer ${accessToken}`)
+                    .send(body);
+                expect(response.statusCode).toBe(200);
+                newPost = response.body.data;
+            });
+        });
+        describe("Exception", () => {
+            test("invalid title - empty, length", async () => {
+                // empty title
+                await testTitleLength("");
+                // short length 2
+                await testTitleLength("aaaa");
+            });
+            test("invalid description - empty, length", async () => {
+                // empty title
+                await testDescLength("");
+                // short length 2
+                await testDescLength("aaaaaaaaa");
+            });
+            async function testTitleLength(title: string) {
                 body.title = title;
                 const response = await request(app)
                     .post(`/api/boards/${board.boardId}/posts`)
                     .set("Authorization", `Bearer ${accessToken}`)
                     .send(body);
-                expect(response.statusCode).toBe(expectedStatusCode);
+                expect(response.statusCode).toBe(400);
             }
 
-            // empty title
-            await testTitleLength("", 400);
-
-            // short length 2
-            await testTitleLength("aaaa", 400);
-        });
-        test("invalid description - empty, length", async () => {
-            async function testDescLength(desc: string, expectedStatusCode: number) {
+            async function testDescLength(desc: string) {
                 body.description = desc;
                 const response = await request(app)
                     .post(`/api/boards/${board.boardId}/posts`)
                     .set("Authorization", `Bearer ${accessToken}`)
                     .send(body);
-                expect(response.statusCode).toBe(expectedStatusCode);
+
+                expect(response.statusCode).toBe(400);
             }
-
-            // empty title
-            await testDescLength("", 400);
-
-            // short length 2
-            await testDescLength("aaaaaaaaa", 400);
         });
     });
-});
+
+    describe("Get Post & Post list API", () => {
+        describe("성공", () => {
+            test("Get - api/boards/:boardId/posts", async () => {
+                const response: any = await request(app)
+                    .get(`/api/boards/${board.boardId}/posts`)
+                    .set("Authorization", `Bearer ${accessToken}`)
+                expect(response.statusCode).toBe(200);
+            });
+            test("Get - api/boards/:boardId/posts/:postId", async () => {
+                const response: any = await request(app)
+                    .get(`/api/boards/${board.boardId}/posts/${newPost.postId}`)
+                    .set("Authorization", `Bearer ${accessToken}`)
+                expect(response.statusCode).toBe(200);
+                const actualPost: Post = response.body.data
+                expect(actualPost.postId).toEqual(newPost.postId)
+                expect(actualPost.title).toEqual(newPost.title)
+                expect(actualPost.description).toEqual(newPost.description)
+                expect(actualPost.createdAt).toEqual(newPost.createdAt)
+            });
+        });
+        describe("Exception", () => {
+            test("invalid token", async () => {
+                let response: any = await request(app).get(`/api/boards/${board.boardId}/posts`)
+                expect(response.statusCode).toBe(401);
+                response = await request(app).get(`/api/boards/${board.boardId}/posts/${newPost.postId}`)
+                expect(response.statusCode).toBe(401);
+            });
+            test("not found", async () => {
+                const response: any = await request(app)
+                    .get(`/api/boards/${board.boardId}/posts/${newPost.postId + 100}`)
+                    .set("Authorization", `Bearer ${accessToken}`)
+                expect(response.statusCode).toBe(404);
+            });
+        });
+    });
+
+    describe("Update Post API", () => {
+        const sendUpdatePost = async (body: any) => {
+            return await request(app)
+                .put(`/api/boards/${board.boardId}/posts/${newPost.postId}`)
+                .set("Authorization", `Bearer ${accessToken}`)
+                .send(body);
+        }
+        describe("성공", () => {
+            test("Put - api/boards/:boardId/posts/:postId", async () => {
+
+                // check only changed title
+                let changedTitle = "changed title"
+                let res: any = await sendUpdatePost({ title: changedTitle })
+                expect(res.statusCode).toBe(200);
+                let actualPost: Post = res.body.data;
+                expect(actualPost.title).toEqual(changedTitle)
+                expect(actualPost.description).toEqual(newPost.description)
+
+                // check only changed description
+                let changedDescription = "changed description for test!"
+                res = await sendUpdatePost({ description: changedDescription })
+                expect(res.statusCode).toBe(200);
+                actualPost = res.body.data;
+                expect(actualPost.description).toEqual(changedDescription)
+                expect(actualPost.title).toEqual(changedTitle)
+
+                // changed both
+                changedTitle = "last changed title"
+                changedDescription = "last changed description"
+                res = await sendUpdatePost({ title: changedTitle, description: changedDescription })
+                expect(res.statusCode).toBe(200);
+                actualPost = res.body.data;
+                expect(actualPost.title).toEqual(changedTitle)
+                expect(actualPost.description).toEqual(changedDescription)
+                newPost = actualPost;
+            });
+        });
+        describe("Exception", () => {
+            test("invalid title", async () => {
+                const response: any = await sendUpdatePost({ title: "1234" })
+                expect(response.statusCode).toBe(400);
+
+            });
+            test("invalid description", async () => {
+                const response: any = await sendUpdatePost({ description: "123456789" })
+                expect(response.statusCode).toBe(400);
+            });
+            test("invalid token", async () => {
+                let response: any = await request(app).put(`/api/boards/${board.boardId}/posts/${newPost.postId}`)
+                expect(response.statusCode).toBe(401);
+            });
+        });
+    });
+
+    describe("Delete Post API", () => {
+        describe("성공", () => {
+            test("Delete - api/boards/:boardId/posts/:postId", async () => {
+                const res: any = await request(app)
+                    .delete(`/api/boards/${board.boardId}/posts/${newPost.postId}`)
+                    .set("Authorization", `Bearer ${accessToken}`)
+                expect(res.statusCode).toBe(200);
+            });
+        });
+        describe("Exception", () => {
+            test("not found", async () => {
+                const res: any = await request(app)
+                    .delete(`/api/boards/${board.boardId}/posts/${newPost.postId + 100}`)
+                    .set("Authorization", `Bearer ${accessToken}`)
+                expect(res.statusCode).toBe(404);
+
+            });
+            test("invalid token", async () => {
+                const res: any = await request(app)
+                    .delete(`/api/boards/${board.boardId}/posts/${newPost.postId}`)
+                expect(res.statusCode).toBe(401);
+            });
+        });
+    });
+})
--- tests/services/user.test.ts
+++ tests/services/user.test.ts
@@ -14,202 +14,206 @@ let createdUser: userBody;
 let accessToken: string;
 let refreshToken: string;
 
-describe("Signup API", () => {
-    let body: userBody;
-    beforeEach(() => {
-        body = {
-            id: "test1234",
-            password: "test5678",
-            username: "testuser",
-            email: "test@a.com",
-        };
+
+describe("User API", () => {
+    describe("Signup API", () => {
+        let body: userBody;
+        beforeEach(() => {
+            body = {
+                id: "test1234",
+                password: "test5678",
+                username: "testuser",
+                email: "test@a.com",
+            };
+        });
+
+        describe("성공", () => {
+            test("Post - api/signup", async () => {
+                const response: any = await request(app).post("/api/signup").send(body);
+                expect(response.statusCode).toBe(200);
+                createdUser = response.body.data;
+            });
+        });
+        describe("Exception", () => {
+            test("empty id", async () => {
+                body.id = "";
+                let response: any = await request(app).post("/api/signup").send(body);
+                expect(response.statusCode).toBe(400);
+            });
+            test("empty password", async () => {
+                body.password = "";
+                let response: any = await request(app).post("/api/signup").send(body);
+                expect(response.statusCode).toBe(400);
+            });
+            test("empty username", async () => {
+                body.username = "";
+                let response: any = await request(app).post("/api/signup").send(body);
+                expect(response.statusCode).toBe(400);
+            });
+            test("empty & invalid email", async () => {
+                body.email = "";
+                let response: any = await request(app).post("/api/signup").send(body);
+                expect(response.statusCode).toBe(400);
+
+                body.email = "adga12113";
+                response = await request(app).post("/api/signup").send(body);
+                expect(response.statusCode).toBe(400);
+            });
+        });
     });
 
-    describe("성공", () => {
-        test("Post - api/signup", async () => {
-            const response: any = await request(app).post("/api/signup").send(body);
-            expect(response.statusCode).toBe(200);
-            createdUser = response.body.data;
+    describe("Login API", () => {
+        let body: any;
+        beforeEach(() => {
+            body = {
+                id: "test1234",
+                password: "test5678",
+            };
         });
-    });
-    describe("Exception", () => {
-        test("empty id", async () => {
-            body.id = "";
-            let response: any = await request(app).post("/api/signup").send(body);
-            expect(response.statusCode).toBe(400);
-        });
-        test("empty password", async () => {
-            body.password = "";
-            let response: any = await request(app).post("/api/signup").send(body);
-            expect(response.statusCode).toBe(400);
-        });
-        test("empty username", async () => {
-            body.username = "";
-            let response: any = await request(app).post("/api/signup").send(body);
-            expect(response.statusCode).toBe(400);
-        });
-        test("empty & invalid email", async () => {
-            body.email = "";
-            let response: any = await request(app).post("/api/signup").send(body);
-            expect(response.statusCode).toBe(400);
+        describe("성공", () => {
+            test("Post - /api/login", async () => {
+                const response: any = await request(app).post(`/api/login`).send(body);
+                expect(response.statusCode).toBe(200);
 
-            body.email = "adga12113";
-            response = await request(app).post("/api/signup").send(body);
-            expect(response.statusCode).toBe(400);
+                const result = response.body.data;
+                accessToken = result.accessToken;
+                refreshToken = result.refreshToken;
+                expect(result.accessToken).not.toBeNull();
+                expect(result.refreshToken).not.toBeNull();
+            });
         });
-    });
-});
+        describe("Exception", () => {
+            test("empty, invalid userID", async () => {
+                // empty case
+                body.id = "";
+                let response: any = await request(app).post(`/api/login`).send(body);
+                expect(response.statusCode).toBe(400);
 
-describe("Login API", () => {
-    let body: any;
-    beforeEach(() => {
-        body = {
-            id: "test1234",
-            password: "test5678",
-        };
-    });
-    describe("성공", () => {
-        test("Post - /api/login", async () => {
-            const response: any = await request(app).post(`/api/login`).send(body);
-            expect(response.statusCode).toBe(200);
+                // invalid id case
+                body.id = "adnklal131";
+                response = await request(app).post(`/api/login`).send(body);
+                expect(response.statusCode).toBe(404);
+            });
+            test("empty, invalid password", async () => {
+                // empty case
+                body.password = "";
+                let response: any = await request(app).post(`/api/login`).send(body);
+                expect(response.statusCode).toBe(400);
 
-            const result = response.body.data;
-            accessToken = result.accessToken;
-            refreshToken = result.refreshToken;
-            expect(result.accessToken).not.toBeNull();
-            expect(result.refreshToken).not.toBeNull();
+                // invalid password case
+                body.password = "13nlflaqp1";
+                response = await request(app).post(`/api/login`).send(body);
+                expect(response.statusCode).toBe(404);
+            });
         });
     });
-    describe("Exception", () => {
-        test("empty, invalid userID", async () => {
-            // empty case
-            body.id = "";
-            let response: any = await request(app).post(`/api/login`).send(body);
-            expect(response.statusCode).toBe(400);
 
-            // invalid id case
-            body.id = "adnklal131";
-            response = await request(app).post(`/api/login`).send(body);
-            expect(response.statusCode).toBe(404);
-        });
-        test("empty, invalid password", async () => {
-            // empty case
-            body.password = "";
-            let response: any = await request(app).post(`/api/login`).send(body);
-            expect(response.statusCode).toBe(400);
+    describe("Get User API", () => {
+        describe("성공", () => {
+            test("Get - /api/users/{id}", async () => {
+                const response: any = await request(app)
+                    .get(`/api/users/${createdUser.userId}`)
+                    .set("Authorization", `Bearer ${accessToken}`);
+                expect(response.statusCode).toBe(200);
 
-            // invalid password case
-            body.password = "13nlflaqp1";
-            response = await request(app).post(`/api/login`).send(body);
-            expect(response.statusCode).toBe(404);
+                const result: userBody = response.body.data;
+                expect(result.id).toEqual(createdUser.id);
+                expect(result.password).toBeUndefined();
+                expect(result.username).toEqual(createdUser.username);
+                expect(result.email).toEqual(createdUser.email);
+            });
+        });
+        describe("Exception", () => {
+            test("not found user", async () => {
+                const response: any = await request(app)
+                    .get(`/api/users/${createdUser.userId! + 100}`)
+                    .set("Authorization", `Bearer ${accessToken}`);
+                expect(response.statusCode).toBe(404);
+            });
         });
     });
-});
 
-describe("Get User API", () => {
-    describe("성공", () => {
-        test("Get - /api/users/{id}", async () => {
-            const response: any = await request(app)
-                .get(`/api/users/${createdUser.userId}`)
-                .set("Authorization", `Bearer ${accessToken}`);
-            expect(response.statusCode).toBe(200);
+    describe("Update User API", () => {
+        let body: userBody;
+        beforeEach(() => {
+            body = {
+                userId: 0, // not used this test
+                id: "", // not used this test
+                username: "",
+                password: "",
+                email: "",
+            };
+        });
+        describe("성공", () => {
+            test(`Put - /api/users/{id}) - username`, async () => {
+                body.username = "changeduser123";
+                delete body.email;
+                const response: any = await request(app)
+                    .put(`/api/users/${createdUser.userId}`)
+                    .set("Authorization", `Bearer ${accessToken}`)
+                    .send(body);
+                expect(response.statusCode).toBe(200);
 
-            const result: userBody = response.body.data;
-            expect(result.id).toEqual(createdUser.id);
-            expect(result.password).toBeUndefined();
-            expect(result.username).toEqual(createdUser.username);
-            expect(result.email).toEqual(createdUser.email);
-        });
-    });
-    describe("Exception", () => {
-        test("not found user", async () => {
-            const response: any = await request(app)
-                .get(`/api/users/${createdUser.userId! + 100}`)
-                .set("Authorization", `Bearer ${accessToken}`);
-            expect(response.statusCode).toBe(404);
-        });
-    });
-});
+                const result: userBody = response.body.data;
+                expect(result.username).toEqual(body.username);
+            });
+            test(`Put - /api/users/{id}) - password`, async () => {
+                body.password = "changeduser313";
+                delete body.email;
+                const response: any = await request(app)
+                    .put(`/api/users/${createdUser.userId}`)
+                    .set("Authorization", `Bearer ${accessToken}`)
+                    .send(body);
+                expect(response.statusCode).toBe(200);
+            });
+            test(`Put - /api/users/{id}) - email`, async () => {
+                body.email = "changeduser@naver.com";
+                const response: any = await request(app)
+                    .put(`/api/users/${createdUser.userId}`)
+                    .set("Authorization", `Bearer ${accessToken}`)
+                    .send(body);
+                expect(response.statusCode).toBe(200);
 
-describe("Update User API", () => {
-    let body: userBody;
-    beforeEach(() => {
-        body = {
-            userId: 0, // not used this test
-            id: "", // not used this test
-            username: "",
-            password: "",
-            email: "",
-        };
+                const result: userBody = response.body.data;
+                expect(result.email).toEqual(body.email);
+            });
+        });
+        describe("Exception", () => {
+            test("invalid email", async () => {
+                body.email = "invalidemailformat";
+                const response: any = await request(app)
+                    .put(`/api/users/${createdUser.userId}`)
+                    .set("Authorization", `Bearer ${accessToken}`)
+                    .send(body);
+                expect(response.statusCode).toBe(400);
+            });
+            test("wrong authenticated", async () => {
+                body.email = "changeduser@naver.com";
+                const response: any = await request(app)
+                    .put(`/api/users/${createdUser.userId! + 100}`)
+                    .send(body);
+                expect(response.statusCode).toBe(401);
+            });
+        });
     });
-    describe("성공", () => {
-        test(`Put - /api/users/{id}) - username`, async () => {
-            body.username = "changeduser123";
-            delete body.email;
-            const response: any = await request(app)
-                .put(`/api/users/${createdUser.userId}`)
-                .set("Authorization", `Bearer ${accessToken}`)
-                .send(body);
-            expect(response.statusCode).toBe(200);
 
-            const result: userBody = response.body.data;
-            expect(result.username).toEqual(body.username);
+    describe("Delete User API", () => {
+        describe("성공", () => {
+            test(`Delete - /api/users/{id})`, async () => {
+                const response: any = await request(app)
+                    .delete(`/api/users/${createdUser.userId}`)
+                    .set("Authorization", `Bearer ${accessToken}`);
+                expect(response.statusCode).toBe(200);
+            });
         });
-        test(`Put - /api/users/{id}) - password`, async () => {
-            body.password = "changeduser313";
-            delete body.email;
-            const response: any = await request(app)
-                .put(`/api/users/${createdUser.userId}`)
-                .set("Authorization", `Bearer ${accessToken}`)
-                .send(body);
-            expect(response.statusCode).toBe(200);
+        describe("Exception", () => {
+            test("not found user", async () => {
+                const response: any = await request(app)
+                    .delete(`/api/users/${createdUser.userId}`)
+                    .set("Authorization", `Bearer ${accessToken}`);
+                expect(response.statusCode).toBe(404);
+            });
         });
-        test(`Put - /api/users/{id}) - email`, async () => {
-            body.email = "changeduser@naver.com";
-            const response: any = await request(app)
-                .put(`/api/users/${createdUser.userId}`)
-                .set("Authorization", `Bearer ${accessToken}`)
-                .send(body);
-            expect(response.statusCode).toBe(200);
+    });
+})
 
-            const result: userBody = response.body.data;
-            expect(result.email).toEqual(body.email);
-        });
-    });
-    describe("Exception", () => {
-        test("invalid email", async () => {
-            body.email = "invalidemailformat";
-            const response: any = await request(app)
-                .put(`/api/users/${createdUser.userId}`)
-                .set("Authorization", `Bearer ${accessToken}`)
-                .send(body);
-            expect(response.statusCode).toBe(400);
-        });
-        test("wrong authenticated", async () => {
-            body.email = "changeduser@naver.com";
-            const response: any = await request(app)
-                .put(`/api/users/${createdUser.userId! + 100}`)
-                .send(body);
-            expect(response.statusCode).toBe(401);
-        });
-    });
-});
-
-describe("Delete User API", () => {
-    describe("성공", () => {
-        test(`Delete - /api/users/{id})`, async () => {
-            const response: any = await request(app)
-                .delete(`/api/users/${createdUser.userId}`)
-                .set("Authorization", `Bearer ${accessToken}`);
-            expect(response.statusCode).toBe(200);
-        });
-    });
-    describe("Exception", () => {
-        test("not found user", async () => {
-            const response: any = await request(app)
-                .delete(`/api/users/${createdUser.userId}`)
-                .set("Authorization", `Bearer ${accessToken}`);
-            expect(response.statusCode).toBe(404);
-        });
-    });
-});
