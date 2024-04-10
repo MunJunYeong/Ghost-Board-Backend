200737b330d77c82b7fb4159cf8c75776e95fed6
MUNJUNYEONG <msw711666@naver.com>
2024-04-10 23:11:27 +0900

    email 인증 interface 추가 및 html template 구현

---
+++ src/common/utils/mailer.ts
@@ -0,0 +1,38 @@
+import nodeMailer from "nodemailer"
+import crypto from "crypto";
+import ejs from 'ejs';
+import path from "path";
+
+let transporter = nodeMailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASSWORD,
+    },
+});
+
+export const sendMail = async (userEmail: string, code: number) => {
+    // const code = crypto.randomBytes(3).toString('hex');
+
+    let emailTemplate;
+
+    // ejs render할 때 상대경로를 인식하지 못함.
+    const absolutePath = path.join(__dirname, 'registerVerify.ejs');
+    ejs.renderFile(absolutePath, { email: userEmail, code: code }, (err, data) => { //ejs mapping
+        if (err) {
+            console.error(err)
+        }
+        emailTemplate = data;
+    });
+
+    let info = await transporter.sendMail({
+        from: `코어실록 이메일 인증`,
+        to: userEmail,
+        subject: '코어실록 회원가입 인증 메일입니다.',
+        html: emailTemplate,
+    });
+    return info
+}
--- src/routes/anonymous.routes.ts
+++ src/routes/anonymous.routes.ts
@@ -1,8 +1,9 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 
 import anonymousController from "@controllers/anonymous/anonymous.controller";
 import { validationMiddleware } from "@middlewares/requestValidate";
 import * as dto from "@controllers/anonymous/dto/anonymous.dto";
+import { sendMail } from "@utils/mailer";
 
 class AnonymousRoutes {
     router = Router();
@@ -18,6 +19,16 @@ class AnonymousRoutes {
         this.router.post("/login", validationMiddleware(dto.LoginReqDTO), this.controller.login);
 
         this.router.post("/refresh", this.controller.refresh);
+
+        this.router.post("/test", async (req: Request, res: Response) => {
+            const result = await sendMail("msw711666@naver.com")
+            res.status(200).json({
+                status: 'Success',
+                code: 200,
+                message: 'Sent Auth Email',
+                data: result
+            });
+        });
     }
 }
 
